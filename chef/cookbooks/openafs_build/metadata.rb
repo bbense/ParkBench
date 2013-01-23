e67fbe75112c07646f55c1f6acef8941aceb7e9c
Booker C. Bense <bbense@gmail.com>
2013-01-22 16:22:06 -0800

    Fixing FC007 error and test clean up

--- chef/cookbooks/openafs_build/metadata.rb
+++ chef/cookbooks/openafs_build/metadata.rb
@@ -5,4 +5,15 @@ license          "MIT"
 description      "Installs/Configures openafs_build"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+recipe            "default", "Builds OpenAFS from source code"
 
+
+%w{ centos debian fedora redhat scientific ubuntu  }.each do |os|
+  supports os
+end
+
+# supports "mac_os_x", ">= 10.6.0"
+
+%w{ build-essential git  }.each do |cookbook|
+  depends cookbook
+end
